7159c5bb6bd4bb0daf91dbc967dc2494f3ac4e21
Saurabh Charde <saurabhchardereal@gmail.com>
2020-05-30 13:18:11 +0530

    X00T: Move ZenParts related stuff to its own init file
    * The purpose of doing this is to have a seperate init.*.rc file for permissions and stuff related (but not limited) to ZenParts.
    
    * Basically, any perf or ZenParts releated stuff will go in here coz it looks cleaner and nice
    
    Signed-off-by: Saurabh Charde <saurabhchardereal@gmail.com>

--- device.mk
+++ device.mk
@@ -377,6 +377,7 @@ PRODUCT_PACKAGES += \
     init.qcom.post_boot.sh \
     init.qcom.sh \
     init.qcom.usb.sh \
+    init.zenparts.sh \
     move_time_data.sh \
     move_wifi_data.sh \
     fix_baseband.sh \
@@ -385,6 +386,7 @@ PRODUCT_PACKAGES += \
     init.qcom.rc \
     init.qcom.usb.rc \
     init.target.rc \
+    init.zenparts.rc \
     ueventd.qcom.rc
 
 # RenderScript HAL
--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -28,6 +28,14 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE       := init.zenparts.sh
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := bin/init.zenparts.sh
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE       := move_time_data.sh
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
@@ -90,6 +98,14 @@ LOCAL_MODULE       := init.target.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.target.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.zenparts.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.zenparts.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
